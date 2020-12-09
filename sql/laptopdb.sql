d5b6b0bdfd0e8c726cafbac96f2c3fb653644437
Tran Huu Anh <huuanhpro00@gmail.com>
2020-12-09 22:40:16 +0700

    update sql

--- sql/laptopdb.sql
+++ sql/laptopdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 10, 2020 lúc 09:06 AM
+-- Thời gian đã tạo: Th12 09, 2020 lúc 04:39 PM
 -- Phiên bản máy phục vụ: 10.4.6-MariaDB
 -- Phiên bản PHP: 7.3.9
 
@@ -39,7 +39,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `username`, `password`) VALUES
-(3, 'huuanh', '202cb962ac59075b964b07152d234b70');
+(4, 'quang', '065d64f121f60917a6ed9a72184d6954'),
+(5, 'huuanh', '5625215db2bbf8fa28f3f32eb2a85d9a');
 
 -- --------------------------------------------------------
 
@@ -65,8 +66,9 @@ INSERT INTO `brands` (`id`, `brand`) VALUES
 (6, 'HP'),
 (7, 'ASUS'),
 (8, 'MSI'),
-(9, 'RAZER'),
-(10, 'ACER');
+(9, 'BKAV'),
+(10, 'ACER'),
+(13, 'SAMSUNG');
 
 -- --------------------------------------------------------
 
@@ -90,7 +92,8 @@ CREATE TABLE `payment` (
 INSERT INTO `payment` (`id`, `name`, `phone`, `address`, `total`, `status`) VALUES
 (7, 'trần hữu anh', '0945586004', '6lk4b khu đô thị mỗ lao', 64970000, 'UNPAID'),
 (8, 'Đức Hùng', '0981434012', '341 Nguyễn Trãi', 87540000, 'UNPAID'),
-(9, 'Đức Hùng', '0981434012', '341 Nguyễn Trãi', 87540000, 'UNPAID');
+(9, 'Đức Hùng', '0981434012', '341 Nguyễn Trãi', 87540000, 'UNPAID'),
+(10, 'trần hữu anh', '0945586004', '6lk4b khu đô thị mỗ lao', 12400000, 'UNPAID');
 
 -- --------------------------------------------------------
 
@@ -114,14 +117,13 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `name`, `image`, `brand`, `description`, `quantity`, `price`, `type`) VALUES
-(1, 'Laptop Acer Aspire 3 A315-56-59XY - Intel Core i5', 'AcerAspire3.png', 'ACER', '                             	                             	                             	                             	Laptop cao cấp sang trọng giá thành hợp lý\r\n                             \r\n                             \r\n                             \r\n                             ', 20, 11990000, 'SCHOOL OFFICE'),
-(2, 'Dell Precision M4800 Intel Core i7 MQ', 'DellPrecisionM4800.png', 'DELL', '                             	Máy trạm cấu hình tốt phù hợp với các đối tượng có nhu cầu sử dụng kỹ thuật đồ họa                             \r\n                             \r\n                             ', 15, 14200000, 'GRAPHICS DESIGN'),
-(3, 'Laptop cao cấp HP Folio 9480m - Intel Core i5', 'HPFolio9480m.png', 'HP', '                             	Laptop cấu hình tốt giá cả phải chăng\r\n                             \r\n                             ', 13, 7150000, 'OLD'),
-(4, 'Laptop Lenovo Legion 5 15ARH05 82B500FXVN - AMD Ryzen 5', 'LenovoLegion5.png', 'LENOVO', '                             	Màn hình 300 nits, 144Hz, 100% sRGB cấu hình R5-4600H, GTX 1650, 16GB RAM, SSD 512GB NVMe\r\n                             \r\n                             \r\n                             \r\n                             ', 14, 22700000, 'GAMING'),
-(5, 'Laptop Dell XPS 9500 - Intel Core i7', 'DellXPS9500.jpg', 'APPLE', '                             	                             	Laptop Dell XPS cấu hình cao, màn 15 inch Chip i7 10750H, card rời RTX 1650Ti 4GB Bảo hành 12 tháng\r\n                             \r\n                             \r\n                             \r\n                             \r\n                             ', 12, 51990000, 'DELUXE'),
-(7, 'Asus Vivobook', 'AsusVivobook.png', 'ASUS', 'Thiết kế sang trọng              \r\n                             ', 23, 21390000, 'DELUXE'),
-(8, 'Laptop LG Gram 2020 14ZD90N-V.AX55A5 - Hàng chính hãng', 'LG_Gram.png', 'LG', '                             	                             	                             	                             	Bảo hành 12 tháng chính hãng - Xem chính sách\r\n\r\n✅Giá ở trên đã bao gồm 10% VAT\r\n\r\n✅ MIỄN PHÍ GIAO HÀNG TẬN NHÀ\r\n\r\n- Với đơn hàng < 4.000.000 đồng: Miễn phí giao hàng cho đơn hàng < 5km tính từ cửa hàng Laptop88 gần nhất\r\n\r\n- Với đơn hàng > 4.000.000 đồng: Miễn phí giao hàng (khách hàng chịu phí bảo hiểm hàng hóa nếu có)                             	\r\n                             \r\n                             \r\n                             \r\n                             \r\n                             ', 8, 26490000, 'DELUXE'),
-(9, 'Dell XPS 13 9370 - Intel Core i7', 'DellXPS9370.png', 'DELL', '                             	                             	                             	                             	Laptop mỏng nhẹ cao cấp nhất của Dell\r\nCấu hình cao, Giá cực tốt\r\nBảo hành 12 tháng   \r\nRAM:8GB\r\nChip:Core I7\r\nCard:NVIDIA GTX1050\r\nỔ cứng:SSD512GB                          	\r\n                             \r\n                             \r\n                             \r\n                             \r\n                             ', 16, 23500000, 'DELUXE');
+(20, 'Acer Aspire 3', 'AcerAspire3.png', 'ACER', '                             	Mẫu laptop hiện đại                       	\r\n                             \r\n                             \r\n                             ', 23, 12300000, 'SCHOOL OFFICE'),
+(21, 'Asus Pro 1440 FA', 'AsusProP1440FA.png', 'ASUS', ' Laptop chính hãng chất lượng cao                    	\r\n                             \r\n                             ', 5, 18000000, 'GRAPHICS DESIGN'),
+(22, 'Asus Vivobook', 'AsusVivobook.png', 'ASUS', ' Laptop cao cấp sang trọng                        	\r\n                             \r\n                             ', 3, 27500000, 'DELUXE'),
+(23, 'Dell Precision M4800', 'DellPrecisionM4800.png', 'DELL', '                             	    Laptop gaming cấu hình cao                         	\r\n                             \r\n                             ', 12, 17600000, 'GAMING'),
+(24, 'HP Folio9480n', 'HPFolio9480m.png', 'HP', '                             	    Laptop dành cho dân văn phòng                         	\r\n                             \r\n                             ', 21, 11000000, 'SCHOOL OFFICE'),
+(25, 'Lenovo IdeapadGaming3', 'LenovoIdeapadGaming3.png', 'LENOVO', '                             	    Laptop rẻ mới nhập                         	\r\n                             \r\n                             ', 14, 9500000, 'SCHOOL OFFICE'),
+(28, 'Dell XPS 9500', 'DellXPS9500.jpg', 'DELL', '                             	Máy bền đẹp chất lượng cao                             	\r\n                             \r\n                             ', 11, 7300000, 'OLD');
 
 -- --------------------------------------------------------
 
@@ -145,7 +147,8 @@ INSERT INTO `product_payment` (`id`, `productId`, `paymentId`, `quantity`) VALUE
 (9, 8, 7, 2),
 (10, 3, 8, 1),
 (11, 2, 8, 2),
-(12, 5, 8, 1);
+(12, 5, 8, 1),
+(13, 20, 10, 1);
 
 -- --------------------------------------------------------
 
@@ -168,6 +171,30 @@ INSERT INTO `type` (`id`, `type`) VALUES
 (3, 'GRAPHICS DESIGN'),
 (4, 'GAMING'),
 (5, 'DELUXE');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(6) NOT NULL,
+  `username` varchar(25) COLLATE utf8_bin NOT NULL,
+  `password` varchar(100) COLLATE utf8_bin NOT NULL,
+  `fullname` varchar(25) COLLATE utf8_bin NOT NULL,
+  `address` varchar(25) COLLATE utf8_bin NOT NULL,
+  `phone` varchar(10) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Đang đổ dữ liệu cho bảng `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `fullname`, `address`, `phone`) VALUES
+(32, 'huyen', 'dba2b86b489924ba4503a523bac2dcbe', 'Trần Thu Huyền', '6lk4b khu đô thị mỗ lao', '0945586003'),
+(33, 'huong', '3f414d9749c322ff0c81e8306449da04', 'Thu Hương', '6lk4b khu đô thị mỗ lao', '0914124342'),
+(36, 'huuanh', '5625215db2bbf8fa28f3f32eb2a85d9a', 'Nguyễn Hữu Anh', '423 Trần Phú', '0932423843');
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -210,6 +237,12 @@ ALTER TABLE `type`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Chỉ mục cho bảng `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT cho các bảng đã đổ
 --
 
@@ -217,37 +250,43 @@ ALTER TABLE `type`
 -- AUTO_INCREMENT cho bảng `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT cho bảng `brands`
 --
 ALTER TABLE `brands`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT cho bảng `payment`
 --
 ALTER TABLE `payment`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT cho bảng `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT cho bảng `product_payment`
 --
 ALTER TABLE `product_payment`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT cho bảng `type`
 --
 ALTER TABLE `type`
   MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT cho bảng `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
